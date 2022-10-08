117ecf77956a955b893b83965e98b1034a89d374
daraclare <daraclare@gmail.com>
2022-10-08 16:44:30 +0100

    Initial

---
+++ src/components/common/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const activeStyle = { color: "purple" };
+  return (
+    <nav>
+      <NavLink to="/" activeStyle={activeStyle} exact>
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle} exact>
+        About
+      </NavLink>
+      {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle} exact>
+        Courses
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("app")
+);
