77ee5e7450a05d76fed783dddaeb606837a56de7
daraclare <daraclare@gmail.com>
2022-10-11 16:36:36 +0100

    remove space

--- src/components/SinglePokemon/SinglePokemon.js
+++ src/components/SinglePokemon/SinglePokemon.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import PokemonCardDetails from "../PokemonCards/PokemonCardDetails";
